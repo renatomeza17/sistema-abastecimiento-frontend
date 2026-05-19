3b7b9e694748a57a990c396bd8736e2f3fa0baf2
Mitchell <sihuinchapalacinmitchellbruce@gmail.com>
2026-05-19 14:13:58 -0500

    git

--- src/app/components/layout/main-layout/main-layout.ts
+++ src/app/components/layout/main-layout/main-layout.ts
@@ -2,17 +2,6 @@ import { Component } from '@angular/core';
 
 
 import { RouterOutlet } from '@angular/router';
-<<<<<<< Updated upstream
-import { Header } from '../header/header'; 
-import { Footer } from '../footer/footer';
-import {Sidebar} from '../sidebar/sidebar';
-
-
-
-@Component({
-  selector: 'app-main-layout',
-  imports: [RouterOutlet, Header, Footer, Sidebar],
-=======
 import { Sidebar } from '../sidebar/sidebar';
 import { HeaderComponent } from '../header/header';
 
@@ -20,7 +9,6 @@ import { HeaderComponent } from '../header/header';
   selector: 'app-main-layout',
   standalone: true,
   imports: [RouterOutlet, Sidebar, HeaderComponent],
->>>>>>> Stashed changes
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
--- src/app/components/layout/sidebar/sidebar.ts
+++ src/app/components/layout/sidebar/sidebar.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../services/auth.service'; /* Ajusta los '../' según tu ruta real al servicio */
 
 @Component({
   selector: 'app-sidebar',
