9ac158626c9b407f15efc75ef49501027c0cec0b
Mitchell <sihuinchapalacinmitchellbruce@gmail.com>
2026-05-19 13:48:18 -0500

    mejorar dashbord y header

--- src/app/components/layout/main-layout/main-layout.ts
+++ src/app/components/layout/main-layout/main-layout.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 
 import { RouterOutlet } from '@angular/router';
+<<<<<<< Updated upstream
 import { Header } from '../header/header'; 
 import { Footer } from '../footer/footer';
 import {Sidebar} from '../sidebar/sidebar';
@@ -11,6 +12,15 @@ import {Sidebar} from '../sidebar/sidebar';
 @Component({
   selector: 'app-main-layout',
   imports: [RouterOutlet, Header, Footer, Sidebar],
+=======
+import { Sidebar } from '../sidebar/sidebar';
+import { HeaderComponent } from '../header/header';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet, Sidebar, HeaderComponent],
+>>>>>>> Stashed changes
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
