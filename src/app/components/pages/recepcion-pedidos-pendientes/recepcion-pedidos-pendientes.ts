23582b137ced6a2c41db0db89b54bb73f9086c78
Alisson Cabanillas <alisson.cabanillas@unmsm.edu.pe>
2026-06-19 18:36:34 -0500

    Mejorar filtros, tabla y flujo de verificación de recepción

---
+++ src/app/components/pages/recepcion-pedidos-pendientes/recepcion-pedidos-pendientes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recepcion-pedidos-pendientes',
+  imports: [],
+  templateUrl: './recepcion-pedidos-pendientes.html',
+  styleUrl: './recepcion-pedidos-pendientes.scss'
+})
+export class RecepcionPedidosPendientesComponent {}
