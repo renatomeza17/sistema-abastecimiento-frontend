adb43bc5b1be85e087d8d1c9a067fdd04f1ec207
Renato Meza <254360150+renatomeza17@users.noreply.github.com>
2026-06-29 01:26:00 -0500

    Environment render

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiUrl: 'https://sistema-abastecimiento.onrender.com'
-    // apiUrl: 'http://localhost:8080'
+    //apiUrl: 'http://localhost:8080'
 };
