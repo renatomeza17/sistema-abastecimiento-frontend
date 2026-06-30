5347f71b841ae503ab6074c623b34967171a79a7
Alisson Cabanillas <alisson.cabanillas@unmsm.edu.pe>
2026-06-30 13:39:49 -0500

    feat: usuarios form y modelo rol

--- src/app/models/rol.ts
+++ src/app/models/rol.ts
@@ -1,5 +1,15 @@
+export interface ModuloPermiso {
+  descripcion: string;
+  url: string;
+  puedeCrear: string;
+  puedeLeer: string;
+  puedeActualizar: string;
+  puedeEliminar: string;
+}
+
 export interface Rol {
     idRol?: number;
     nombre: string;
-
+    descripcion?: string;
+    modulos?: ModuloPermiso[];
 }
